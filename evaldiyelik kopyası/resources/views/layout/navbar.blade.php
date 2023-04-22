bbb051dc3bff98d0d812e9fe113f743b099e7232
haticesezgin <131539164+haticesezgin@users.noreply.github.com>
2023-04-22 18:18:57 +0300

    kitap inceleme sitesi
    Laravel-9.0\Laravel mix  ile oluşturulmuş websitesi
    JS ,HTML,CSS,Bootstrap 5.0 kullanılmıştır.

---
+++ evaldiyelik kopyası/resources/views/layout/navbar.blade.php
@@ -0,0 +1,68 @@
+<div class="row g-0">
+    <div class="col-12 p-2 ps-4 pe-4 m-0">
+        <nav class="navbar navbar-light mt-2 mb-2 p-0">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/"><b>Evladiyelik</b></a>
+                <div class="container">
+                    <header class="d-flex justify-content-center py-3">
+                        <div class="container">
+                            <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+                                <div class="col-md-3 mb-2 mb-md-0">
+                                    <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+                                        <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+                                    </a>
+                                </div>
+
+                                <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                                </ul>
+                                <div class="col-md-3 text-end">
+                                    <button type="button" class="btn btn-outline-primary me-2">Giriş</button>
+
+                                </div>
+                            </header>
+                        </div>
+                        <ul class="nav nav-pills">
+                            <li class="nav-item"><a href="#" class="nav-link active " aria-current ="page">Anasayfa</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link ">Hakkımızda</a></li>
+                            <li class="nav-item"><a href="#" class="nav-link ">Yazarlar</a></li>
+                            <header class="p-3 mb-3 border-bottom">
+                                <div class="container">
+                                    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                                        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+                                            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+                                        </a>
+
+                                        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+
+
+                                        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                                            <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+                                        </form>
+
+                                        <div class="dropdown text-end">
+                                            <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                                <img src="WhatsApp-Image-2023-04-07-at-12..png" alt="mdo" width="32" height="45" class="rounded-circle">
+                                            </a>
+                                            <ul class="dropdown-menu text-small" style="">
+                                                <li><a class="dropdown-item" href="#">Ayarlar</a></li>
+                                                <li><a class="dropdown-item" href="#">Profil</a></li>
+                                                <li><hr class="dropdown-divider"></li>
+                                                <li><a class="dropdown-item" href="#">Çıkış yap</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </header>
+
+                        </ul>
+                    </header>
+                </div>
+
+
+            </div>
+        </nav>
+    </div>
+    <div class="col-12 text-center g-0 border-bottom-black border-top-black background-black" >
+        <img  style="max-height: 420px;" src="/old-books-wallpaper-preview.png" title="My blog main image">
+    </div>
+</div>
